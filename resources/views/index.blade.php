3022d0c9c7b2496eb125d70eb436838eabab407b
Cparfait <clement.parfaitpro@gmail.com>
2022-12-09 16:43:05 +0100

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<html class="dark">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+<body>
+  <!-- Will be black -->
+  <div class="bg-white dark:bg-black">
+    <p>test</p>
+  </div>
+</body>
+</html>
