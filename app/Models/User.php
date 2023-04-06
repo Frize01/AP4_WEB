37735dacadcfe2c8980405f9009cd203ffd77801
T.Sackebandt <tomy49@laposte.net>
2023-04-06 11:46:05 +0200

    Modif User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,26 +1,37 @@
 <?php
-
+ 
 /**
  * Created by Reliese Model.
  */
-
+ 
 namespace App\Models;
-
+ 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Collection;
+use App\Models\STAFF;
+use App\Models\CLIENT;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\SERVEUR;
+use App\Models\COMMANDE;
+use App\Models\ADMINISTRATEUR;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+ 
 /**
  * Class USER
  * 
- * @property int $ID
- * @property string $NAME
- * @property string $EMAIL
- * @property Carbon|null $EMAIL_VERIFIED_AT
- * @property string $PASSWORD
- * @property string|null $REMEMBER_TOKEN
- * @property Carbon|null $CREATED_AT
- * @property Carbon|null $UPDATED_AT
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $password_resets
  * 
  * @property ADMINISTRATEUR $a_d_m_i_n_i_s_t_r_a_t_e_u_r
  * @property CLIENT $c_l_i_e_n_t
@@ -30,48 +41,50 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @package App\Models
  */
-class USER extends Model
+class USER extends Authenticatable
 {
+	use HasApiTokens, HasFactory, Notifiable;
+ 
 	protected $table = 'USERS';
-	protected $primaryKey = 'ID';
-	public $timestamps = false;
-
+ 
 	protected $dates = [
-		'EMAIL_VERIFIED_AT',
-		'CREATED_AT',
-		'UPDATED_AT'
+		'email_verified_at'
 	];
-
+ 
+	protected $hidden = [
+		'password',
+		'remember_token'
+	];
+ 
 	protected $fillable = [
-		'NAME',
-		'EMAIL',
-		'EMAIL_VERIFIED_AT',
-		'PASSWORD',
-		'REMEMBER_TOKEN',
-		'CREATED_AT',
-		'UPDATED_AT'
+		'name',
+		'email',
+		'email_verified_at',
+		'password',
+		'remember_token',
+		'password_resets'
 	];
-
+ 
 	public function a_d_m_i_n_i_s_t_r_a_t_e_u_r()
 	{
 		return $this->hasOne(ADMINISTRATEUR::class, 'ID');
 	}
-
+ 
 	public function c_l_i_e_n_t()
 	{
 		return $this->hasOne(CLIENT::class, 'ID');
 	}
-
+ 
 	public function c_o_m_m_a_n_d_e_s()
 	{
 		return $this->hasMany(COMMANDE::class, 'ID');
 	}
-
+ 
 	public function s_e_r_v_e_u_r()
 	{
 		return $this->hasOne(SERVEUR::class, 'ID');
 	}
-
+ 
 	public function s_t_a_f_f()
 	{
 		return $this->hasOne(STAFF::class, 'ID');
