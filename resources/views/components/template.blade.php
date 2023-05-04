dddd781dc4b445422788b4b75cad65fb53853e51
Cparfait <clement.parfaitpro@gmail.com>
2023-05-04 11:54:27 +0200

    Prise en charge responsivité.

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -59,15 +59,15 @@
     <body class="bg-snowgray">
         <nav class="px-2 sm:px-4 py-2.5 bg-[{{$couleur}}] fixed w-full z-20 top-0 left-0 border-b border-gray-200">
             <div class="container flex flex-wrap items-center justify-between mx-auto">
-                <a href="http://127.0.0.1:8000/" class="flex items-center">
+                <a href="/" class="flex items-center">
                     <img src="{{ asset('images/logo-ration-t.png') }}" class="h-9 mr-3 xl:h-16" alt="Ration logo" />
                     <span class="self-center text-xl font-semibold whitespace-nowrap">Ration</span>
                 </a>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false" onclick="document.getElementById('navbar-default').classList.toggle('hidden');">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                <div class="hidden w-full md:block md:w-auto" id="navbar-default" data-collapse>
                     <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium md:border-0 md:bg-white md:dark:bg-gray-900">
                         <li>
                             <a href="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-2 hover:bg-[{{$bg}}]" aria-current="page">Accueil</a>
@@ -136,7 +136,7 @@
                         </ul>
                     </div>
                     <div>
-                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Légal</h2>
+                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
                         <ul class="text-gray-600">
                             <li>
                                 <a href="/condition" class="hover:underline">Condition de vente et d'utilisation</a>
